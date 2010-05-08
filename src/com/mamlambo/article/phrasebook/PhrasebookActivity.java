53ab247bfdb40b9cc16d7c95150131404f5bf00b
mamlambo <mamlambo@a2018580-c634-9b85-7ca7-93ab31f36c88>
2010-05-08 21:43:18 +0000

    Initial checkin

---
+++ src/com/mamlambo/article/phrasebook/PhrasebookActivity.java
@@ -0,0 +1,13 @@
+package com.mamlambo.article.phrasebook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhrasebookActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
